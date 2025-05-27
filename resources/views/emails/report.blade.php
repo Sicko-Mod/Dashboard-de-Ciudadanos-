aa4e589259da8bc636c9f2dd005998567df4455d
Sicko-Mod <aarochanog@gmail.com>
2025-05-26 20:34:33 -0600

    segundo commit

---
+++ resources/views/emails/report.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>{{ __('Daily Report')}}</h1>
+        <p>{{ $reportData }}</p>
+</body>
+</html>
